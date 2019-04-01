da086fdfb3d18120d6598a2299b3d1a491448a92
Uomar7 <uomarearlie7@gmail.com>
2019-04-01 11:06:49 +0300

    buttons put for the functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#btnFunction").click(function(){
+    $("#submit").click(function(){
 
         var answer1 = $("input[name='question1']:checked").val();
         var answer2 = $("input[name='question2']:checked").val();
@@ -8,5 +8,30 @@ $(document).ready(function(){
         var answer5 = $("input[name='question5']:checked").val();
         var answer6 = $("input[name='question6']:checked").val();
  
+        var totalMarks = 0
+
+        if (answer1 == $("#spn").val()) {
+            totalMarks++
+        };
+
+        if (answer2 == $("#boolean").val()) {
+            totalMarks++
+        };
+
+        if (answer3 == $("#HTML").val()) {
+            totalMarks++
+        };
+
+        if (answer4 == $("#content").val()) {
+            totalMarks++
+        };
+
+        if (answer5 == $("#programming").val()) {
+            totalMarks++
+        };
+
+        if (answer6 == $("#script").val()) {
+            totalMarks++
+        };
     })
 })
