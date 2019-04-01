7e4a24b6b28b0093135ffd2f2ae363651ccd5aa3
Uomar7 <uomarearlie7@gmail.com>
2019-04-01 09:53:07 +0300

    background added to test page and color changed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-    $("#btnlink").click(function(){
-        <a href="/home/king/Moringa-school-projects/quizbod/test.html">
+    $("#btnFunction").click(function(){
+
+        var answer1 = $("input[name='question1']:checked").val();
+        var answer2 = $("input[name='question2']:checked").val();
+        var answer3 = $("input[name='question3']:checked").val();
+        var answer4 = $("input[name='question4']:checked").val();
+        var answer5 = $("input[name='question5']:checked").val();
+        var answer6 = $("input[name='question6']:checked").val();
+ 
     })
 })
